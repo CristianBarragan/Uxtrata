df45183ca522ce259ca840728a76abd029f3af1c
cristian.barragan@gmail.com <cristian.barragan@gmail.com>
2017-09-18 20:52:54 +1200

    Cleaning Courses and extending rdlc for courses

--- UxtrataWeb/Controllers/CourseReportController.cs
+++ UxtrataWeb/Controllers/CourseReportController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Reporting.WebForms;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using UxtrataWeb.Business;
@@ -35,6 +38,63 @@ namespace UxtrataWeb.Controllers
         {
             business = new ReportBusiness();
             return Json(business.getReportCourses(id), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public FileContentResult Report()
+        {
+            return (FileContentResult)Session["FileResult"];
+        }
+
+        [HttpPost]
+        public HttpStatusCodeResult Report(string formatId, int courseId)
+        {
+            business = new ReportBusiness();
+            LocalReport lr = new LocalReport();
+            string path = Path.Combine(Server.MapPath("~/Reports"), "StudentReport.rdlc");
+            if (System.IO.File.Exists(path))
+            {
+                lr.ReportPath = path;
+            }
+            else
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ReportDataSource rd = new ReportDataSource("studentReportDataSet", business.getReportCourses(courseId));
+            lr.DataSources.Add(rd);
+            string reportType = formatId;
+            string mimeType;
+            string encoding;
+            string fileNameExtension;
+
+
+
+            string deviceInfo =
+
+            "<DeviceInfo>" +
+            "  <OutputFormat>" + formatId + "</OutputFormat>" +
+            "  <PageWidth>8.5in</PageWidth>" +
+            "  <PageHeight>11in</PageHeight>" +
+            "  <MarginTop>0.5in</MarginTop>" +
+            "  <MarginLeft>1in</MarginLeft>" +
+            "  <MarginRight>1in</MarginRight>" +
+            "  <MarginBottom>0.5in</MarginBottom>" +
+            "</DeviceInfo>";
+
+            Warning[] warnings;
+            string[] streams;
+            byte[] renderedBytes;
+
+            renderedBytes = lr.Render(
+                reportType,
+                deviceInfo,
+                out mimeType,
+                out encoding,
+                out fileNameExtension,
+                out streams,
+                out warnings);
+            Session["FileResult"] = File(renderedBytes, mimeType);
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
         }
 
         private UxtrataWeb.ModelView.SelectListItem empty()
